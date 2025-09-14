ba9d881921898d04f9f6ba51a9e302f4a4089b30
Kaique790 <kiaqbs@gmail.com>
2025-09-14 11:16:04 -0300

    refactor: reading xlsx

--- atividade-final-analise-de-dados/home.py
+++ atividade-final-analise-de-dados/home.py
@@ -1,26 +1,22 @@
 import pandas as pd
 import streamlit as st
 
-import altair as alt
-
-df = pd.read_csv("funcionarios-empresa - EMPRESA.csv")
+df = pd.read_excel("funcionarios-empresa.xlsx")
 pd.DataFrame(df)
 
 df = df.dropna(how="all", axis=1)
 
-def formatBRL (column):
-    df[column] = df[column].str.replace("R\$ ?", "", regex=True)
-    df[column] = df[column].str.replace(".", "")
-    df[column] = df[column].str.replace(",", ".")
-    df[column] = pd.to_numeric(df[column])
+st.set_page_config(
+    page_title="Análise de funcionários",
+)
 
-formatBRL("Salário Líquido")
-formatBRL("Hora Extra (Total.)")
-formatBRL("Imposto de Renda R$")
-formatBRL("Salário Bruto")
+
 
 st.title("Análise dos funcionários")
 st.subheader("Métricas da empresa")
+
+
+st.divider()
 
 options = [
     "Quantos funcionários a empresa possui?",
@@ -29,13 +25,13 @@ options = [
     "Qual funcionário (nome) recebeu a maior gratificação e qual recebeu a menor?",
     "Qual funcionário paga o maior imposto de renda e qual paga o menor?",
     "Qual funcionário tem o maior salário líquido?",
-    "Qual funcionário tem o menor salário líquido?"
+    "Qual funcionário tem o menor salário líquido?",
 ]
 
 qtd_func = df["Nome Completo"].nunique()
 
-liquid_salarys = df["Salário Líquido"] 
-gross_salarys = df["Salário Bruto"] 
+liquid_salarys = df["Salário Líquido"]
+gross_salarys = df["Salário Bruto"]
 
 mean_salary = liquid_salarys.mean()
 
@@ -44,10 +40,10 @@ min_salary = liquid_salarys.min()
 
 selected_option = st.selectbox("Selecione o que deseja:", options)
 
-if(selected_option):
+if selected_option:
     st.subheader(selected_option)
-    
-if(selected_option == options[0]):
+
+if selected_option == options[0]:
     st.metric(label="A empresa possui:", value=f"{qtd_func} funcionários")
 
 if selected_option == options[1]:
@@ -56,31 +52,33 @@ if selected_option == options[1]:
         st.metric(label="Salário liquido médio:", value=f"{mean_salary:.2f}")
     with col2:
         st.metric(label="Maior salário liquido:", value=f"R$ {max_salary:.2f}")
-    with col3: 
+    with col3:
         st.metric(label="Menor salário liquido:", value=f"R$ {min_salary:.2f}")
-    
+
 if selected_option == options[2]:
     total_gratifications = df["Gratificação R$"].sum()
     total_overtime = df["Hora Extra (Total.)"].sum()
-    
+
     total_spent = total_gratifications + total_overtime
     st.metric(label="total gasto:", value=f"R$ {total_spent:.2f}")
 
 if selected_option == options[3]:
     max_gratification = df["Gratificação R$"].max()
     min_gratification = df["Gratificação R$"].min()
-    
-    higher_workers = df[df["Gratificação R$"] == max_gratification]["Nome Completo"]
-    min_workers = df[df["Gratificação R$"] == min_gratification]["Nome Completo"]
-    
+
+    higher_workers = df[df["Gratificação R$"]
+                        == max_gratification]["Nome Completo"]
+    min_workers = df[df["Gratificação R$"] ==
+                     min_gratification]["Nome Completo"]
+
     st.subheader("Funcionários com maior gratificação:")
     col1, col2 = st.columns(2)
-    
+
     st.markdown("---")
-    
+
     st.subheader("Funcionários com menor gratificação:")
     min1, min2 = st.columns(2)
-    
+
     for i, w in enumerate(higher_workers):
         if i % 2 == 0:
             with col1:
@@ -88,7 +86,7 @@ if selected_option == options[3]:
             continue
         with col2:
             st.write(w)
-    
+
     for i, w in enumerate(min_workers):
         if i % 2 == 0:
             with min1:
@@ -96,37 +94,41 @@ if selected_option == options[3]:
             continue
         with min2:
             st.write(w)
-            
-if(selected_option == options[4]):
+
+if selected_option == options[4]:
     max_tax = df["Imposto de Renda R$"].max()
     min_tax = df["Imposto de Renda R$"].min()
-    
-    higher_worker = df[df["Imposto de Renda R$"] == max_tax]["Nome Completo"].iloc[0]
-    min_worker = df[df["Imposto de Renda R$"] == min_tax]["Nome Completo"].iloc[0]
-       
+
+    higher_worker = df[df["Imposto de Renda R$"]
+                       == max_tax]["Nome Completo"].iloc[0]
+    min_worker = df[df["Imposto de Renda R$"]
+                    == min_tax]["Nome Completo"].iloc[0]
+
     st.metric("O funcionário com maior imposto de renda é: ", higher_worker)
     st.metric("O funcionário com menor imposto de renda é: ", min_worker)
 
 if selected_option == options[5]:
     higher_salary_worker = df[df["Salário Líquido"] == max_salary]
+
     name = higher_salary_worker["Nome Completo"].iloc[0]
     salary = higher_salary_worker["Salário Líquido"].iloc[0]
-    
-    st.metric("Funcionário:", name)
-    st.metric("Salário:", f"R$ {salary:.2f}")
-    
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.metric("Funcionário:", name)
+    with col2:
+        st.metric("Salário:", f"R$ {salary:.2f}")
+
 if selected_option == options[6]:
     min_salary_worker = df[df["Salário Líquido"] == min_salary]
+
     name = min_salary_worker["Nome Completo"].iloc[0]
     salary = min_salary_worker["Salário Líquido"].iloc[0]
-    
-    st.metric("Funcionário:", name)
-    st.metric("Salário:", f"R$ {salary:.2f}")
-    
 
-chart = alt.Chart(df).mark_bar().encode(
-    y=alt.Y("Nome Completo", sort='-x'),
-    x="Salário Líquido"
-)
+    col1, col2 = st.columns(2)
 
-st.altair_chart(chart)
+    with col1:
+        st.metric("Funcionário:", name)
+    with col2:
+        st.metric("Salário:", f"R$ {salary:.2f}")
